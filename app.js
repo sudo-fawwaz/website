2063152bd43a3301f84899ee67506f51ff27f73d
sudo-fawwaz <fwwz.id@gmail.com>
2020-07-04 12:51:32 +0700

    First website deploy

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.engine('ejs')
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,28 @@ router.get('/', (req, res, next) => {
 });
 
 // Admin Pages
+router.get('/login', (req, res, next) =>{
+  res.render('admin_pages/login', {title : 'Login'})
+});
+
 router.get('/admin', (req, res, next) =>{
-  res.render('admin_pages/dashboard', {title: "Dashboard"})
+  res.render('admin_pages/dashboard', {title: "Dashboard", topnav: "Halaman Utama"})
 })
 
-router.get('/article/add', (req, res, next) =>{
-  res.render('admin_pages/add_article', {title: "Create Article"})
+router.get('/admin/add', (req, res, next) =>{
+  res.render('admin_pages/add_article', {title: "Create Article", topnav : "Artikel Baru"})
+})
+router.get('/admin/modify', (req, res, next) =>{
+  res.render('admin_pages/edit_article', {title: "Edit Article", topnav : "Edit Artikel"})
+})
+router.get('/admin/delete', (req, res, next) =>{
+  res.render('admin_pages/delete_article', {title: "Delete Article", topnav : "Buang Artikel"})
+})
+
+// Article Pages
+//Kedepannya di ganti article/:param
+router.get('/article/1', (req, res, next) => {
+  res.render('article_pages/article-1', {title : "Aqidah"});
 })
 
 module.exports = router;
